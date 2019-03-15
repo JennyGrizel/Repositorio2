2c9b93bf55dde5796e97ffad5cec11bd7e9c7db9
Jenny_Cerecero <jenny_cerecero@hotmail.com>
2019-03-15 11:37:31 -0600

    agrego funcion

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define pi 3.1415
+#define day 3
+
 /////////esto es una prueba
-void main(
+void main{
 	printf("Hola\r\n");
-)
+}
+
+int suma(value 1, value 2)
+{
+	int suma= value1 + value2;
+}
 /*Se agregaron librerias*/
 //cambio
