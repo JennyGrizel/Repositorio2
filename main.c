a2167f1e6d7dc21dbf181b9008f750e19ef6f550
Jenny_Cerecero <jenny_cerecero@hotmail.com>
2019-03-15 11:39:24 -0600

    cambios2

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define pi 3.15
+
 /////////esto es una prueba
-void main(
+void main(void){
 	printf("Hola\r\n");
-)
+}
+
+int resta(){
+	return 1;
+}
 /*Se agregaron librerias*/
 //cambio
